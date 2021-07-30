882ddc95c85516f2977dc292c67225b5cbc6999d
hutianhang <tianhang.hu@alphayee.com>
2021-07-30 16:49:18 +0800

    时钟展示优化

--- app/src/main/java/com/thssh/smsdispatcher/activity/ClockActivity.java
+++ app/src/main/java/com/thssh/smsdispatcher/activity/ClockActivity.java
@@ -16,20 +16,35 @@ import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.thssh.smsdispatcher.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class ClockActivity extends AppCompatActivity {
 
     private static final String INDEX = ":";
     private static final String TIME_INIT = "--";
+    private static final SimpleDateFormat FORMAT_DATE = new SimpleDateFormat("MM/dd/yyyy");
+    private static final long UPDATE_STEP = 50;
+    private static final Map<Integer, String> WEEK_DIC = new HashMap<>();
+    static {
+        WEEK_DIC.put(1, "周一");
+        WEEK_DIC.put(2, "周二");
+        WEEK_DIC.put(3, "周三");
+        WEEK_DIC.put(4, "周四");
+        WEEK_DIC.put(5, "周五");
+        WEEK_DIC.put(6, "周六");
+        WEEK_DIC.put(7, "周日");
+    }
 
     public static void start(Activity activity) {
         activity.startActivity(new Intent(activity, ClockActivity.class));
     }
 
-    private TextView hours, minutes, seconds, hoursIndex, minutesIndex;
+    private TextView hours, minutes, seconds, hoursIndex, minutesIndex, microSeconds, date;
     private Handler mHandler;
 
     @Override
@@ -58,18 +73,37 @@ public class ClockActivity extends AppCompatActivity {
             updateUI(time);
             mLastSeconds = seconds;
         }
-        toggleVisible(hoursIndex, minutesIndex);
-        mHandler.postDelayed(this::work, 500);
+        setVisible(updateMicroSeconds(time), hoursIndex, minutesIndex);
+        mHandler.postDelayed(this::work, UPDATE_STEP);
+    }
+
+    private boolean updateMicroSeconds(Date time) {
+        int micro = (int) (time.getTime() % 1000);
+        microSeconds.setText(formatMicro(micro));
+        return micro >= 500;
     }
 
     private void updateUI(Date time) {
         hours.setText(format(time.getHours()));
         minutes.setText(format(time.getMinutes()));
         seconds.setText(format(time.getSeconds()));
+        date.setText(String.format("%s %s", FORMAT_DATE.format(time), WEEK_DIC.get(time.getDay())));
+    }
+
+    private String formatMicro(int t) {
+        if (t < 10) {
+            return String.format(Locale.getDefault(), "00%d", t);
+        }
+        if (t < 100) {
+            return String.format(Locale.getDefault(), "0%d", t);
+        }
+        return String.valueOf(t);
     }
 
     private String format(int t) {
-        if (t < 10) return String.format(Locale.getDefault(), "0%d", t);
+        if (t < 10) {
+            return String.format(Locale.getDefault(), "0%d", t);
+        }
         return String.valueOf(t);
     }
 
@@ -79,11 +113,19 @@ public class ClockActivity extends AppCompatActivity {
         }
     }
 
+    private void setVisible(boolean visible, View... views) {
+        for (View view : views) {
+            view.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
     private void initViews() {
         (hours = findViewById(R.id.txt_hour)).setText(TIME_INIT);
         (hoursIndex = findViewById(R.id.txt_hour_split)).setText(INDEX);
         (minutes = findViewById(R.id.txt_minutes)).setText(TIME_INIT);
         (minutesIndex = findViewById(R.id.txt_minutes_split)).setText(INDEX);
         (seconds = findViewById(R.id.txt_second)).setText(TIME_INIT);
+        (microSeconds = findViewById(R.id.txt_micro_seconds)).setText(TIME_INIT);
+        (date = findViewById(R.id.txt_date)).setText("--/--/----");
     }
 }
